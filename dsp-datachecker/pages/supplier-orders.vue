9778da130a014bb5c87cd04fd608e3ec4bc8213a
carpcai <ly.cai@aftership.com>
2020-11-10 16:03:49 +0800

    Fixed.

--- dsp-datachecker/pages/supplier-orders.vue
+++ dsp-datachecker/pages/supplier-orders.vue
@@ -278,7 +278,7 @@ export default {
         // }
       ],
       orderList: [],
-      requestEnv: "dev",
+      requestEnv: "production",
       requestEnvMap: {
         dev: {
           product_url: "",
